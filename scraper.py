f0719613d17dcdbe8b27fb68aba0b7a17f316724
Robert Smith <robertsmith@Roberts-MacBook-Pro.local>
2019-03-01 14:22:08 -0500

    created def to connect to website

--- scraper.py
+++ scraper.py
@@ -1,7 +1,22 @@
 # main file for scraping a website
 
-import requests
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
 from bs4 import BeautifulSoup
+
+
+def connect():
+	url = 'http://www.google.com'
+	response = simple_get(url)
+	if response is not None:
+		return true
+	
+	return false
+
 
 if __name__=='__main__':
 	print('Connecting to the website')
+	
+	is_connected = connect()
+	print('is connected: ' +str(is_connected))
